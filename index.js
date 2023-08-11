b7f203d46f6f9fc37f29d7c33407cc8439ffd700
AngeloAc <euclidescarvalho1904@gmail.com>
2023-08-11 15:44:30 +0100

    insert ws and conect and desconect

--- index.js
+++ index.js
@@ -1,16 +1,187 @@
 // create a server 
-const app = require('./src/app');
 const express = require('express');
+const app = express();
 const http = require('http');
 // require('dotenv').config()
+const cors = require('cors')
+const body_parser = require('body-parser');
+const WebSocket = require('ws');
+
 
 
 
 //used just for test
 // const porta = normalizePort(process.env.PORT || 3000); 
-app.set('port', porta);
+
 
 const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(cors({
+    origin: '*',
+}))
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: false }))
+
+//whatsapp
+const fs = require('fs');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Buttons, List } = require('whatsapp-web.js');
+const QRCODE = require('qrcode')
+let qrCode = "www.oinet.ao";
+let percentual = '0';
+let message = "Whatsapp";
+let auth = false;
+let auth_error = "Erro ao se autenticar.";
+let clientOn = "black"
+
+const SESSION_FILE_PATH = './session.json';
+
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+    puppeteer: {
+        executablePath: '/usr/bin/brave-browser-stable',
+    },
+    authStrategy: new LocalAuth({
+        clientId: "client-one"
+    }),
+    puppeteer: {
+        headless: true,
+    }
+});
+
+
+
+
+wss.on('connection', (ws) => {
+    console.log('Novo cliente conectado');
+  
+   
+    client.on('qr', qr => {
+        //qrcode.generate(qr, {small: true});
+        // qrCode = qr;
+        console.log("QR. CODE IS RUNNING");
+    
+        QRCODE.toDataURL(qr, (err, url) => {
+            if (err) {
+                console.error('Erro ao gerar o código QR:', err);
+                res.status(500).send('Erro ao gerar o código QR');
+            } else {
+                ws.send(JSON.stringify({
+                    qrCode: url,
+                    percentual: percentual,
+                    message: message,
+                    auth: auth,
+                    auth_error: auth_error,
+                    clientOn: clientOn
+        
+                }));
+    
+            }
+        });
+       
+    });
+
+    client.on('authenticated', () => {
+        console.log('Autenticado');
+        auth = true;
+    
+        ws.send(JSON.stringify({
+            qrCode: null,
+            percentual: percentual,
+            message: message,
+            auth: auth,
+            auth_error: auth_error,
+            clientOn: clientOn
+
+        }));
+    });
+
+    
+    
+    // Enviar mensagens para o cliente a cada 5 segundos
+   
+  
+    ws.on('close', () => {
+    //   clearInterval(sendInterval);
+      console.log('Cliente desconectado');
+    });
+  });
+
+client.on('loading_screen', (percent, message) => {
+    console.log('Carregando... ', percent, message);
+    percentual = percent;
+    message = message;
+});
+
+client.on('authenticated', () => {
+    console.log('Autenticado');
+    auth = true;  
+});
+
+
+client.on('ready', () => {
+    console.log('Cliente pronto');
+    clientOn = "red";
+});
+
+
+
+client.initialize();
+
+app.get('/client/logout', (req, res, next)=>{
+    client.logout();
+    console.log('cliente whatsapp desconectado com sucess.')
+    res.status(200).json({
+        message: 'Client disconnected from whatsapp.'
+    });
+})
+
+app.get('/qrcode', (req, res, next) => {
+
+    //Gerar o conteúdo do QR Code
+    // const qrCodeContent = qrCode;
+
+    // Gerar o código QR baseado no conteúdo
+    // QRCODE.toDataURL(qrCodeContent, (err, url) => {
+    //     if (err) {
+    //         console.error('Erro ao gerar o código QR:', err);
+    //         res.status(500).send('Erro ao gerar o código QR');
+    //     } else {
+    //         // console.log("url: " + url);
+    //         // Exibir o código QR no navegador
+    
+    //         // res.send(qrCodeHtml);
+    //         res.status(200).json({
+    //             qrCode: url,
+    //             percentual: percentual,
+    //             message: message,
+    //             auth: auth,
+    //             auth_error: auth_error,
+    //             clientOn: clientOn
+
+    //         })
+    //     }
+    // });
+
+    res.status(200).json({
+        message: 'obaaa'
+    })
+
+});
+
+
+
+app.set('port', porta);
+
+
+
 
 server.listen(porta);
 console.log("Server is running on port 3000");
--- src/app.js
+++ src/app.js
@@ -1,109 +1,113 @@
-const express = require('express');
-// const mongo = require('mongoose');
-// require('dotenv').config()
-const app = express();
-const cors = require('cors')
-const body_parser = require('body-parser');
+// const express = require('express');
+// // const mongo = require('mongoose');
+// // require('dotenv').config()
+// const app = express();
+// const cors = require('cors')
+// const body_parser = require('body-parser');
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(cors({
-    origin: '*',
-}))
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: false }))
 
-//whatsapp
-const fs = require('fs');
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const { Buttons, List } = require('whatsapp-web.js');
-const QRCODE = require('qrcode')
-let qrCode = "www.oinet.ao";
-let percentual = '0';
-let message = "Whatsapp";
-let auth = false;
-let auth_error = "Erro ao se autenticar.";
-let clientOn = "black"
 
-const SESSION_FILE_PATH = './session.json';
 
-let sessionData;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-}
 
-const client = new Client({
-    puppeteer: {
-        executablePath: '/usr/bin/brave-browser-stable',
-    },
-    authStrategy: new LocalAuth({
-        clientId: "client-one"
-    }),
-    puppeteer: {
-        headless: true,
-    }
-});
+// app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.use(cors({
+//     origin: '*',
+// }))
+// app.use(body_parser.json());
+// app.use(body_parser.urlencoded({ extended: false }))
 
-client.on('qr', qr => {
-    //qrcode.generate(qr, {small: true});
-    qrCode = qr;
-    console.log("QR. CODE IS RUNNING")
-});
+// //whatsapp
+// const fs = require('fs');
+// const { Client, LocalAuth } = require('whatsapp-web.js');
+// const { Buttons, List } = require('whatsapp-web.js');
+// const QRCODE = require('qrcode')
+// let qrCode = "www.oinet.ao";
+// let percentual = '0';
+// let message = "Whatsapp";
+// let auth = false;
+// let auth_error = "Erro ao se autenticar.";
+// let clientOn = "black"
 
-client.on('loading_screen', (percent, message) => {
-    console.log('Carregando... ', percent, message);
-    percentual = percent;
-    message = message;
-});
+// const SESSION_FILE_PATH = './session.json';
 
-client.on('authenticated', () => {
-    console.log('Autenticado');
-    auth = true;
-});
+// let sessionData;
+// if (fs.existsSync(SESSION_FILE_PATH)) {
+//     sessionData = require(SESSION_FILE_PATH);
+// }
 
-client.on('auth_failure', msg => {
-    // Fired if session restore was unsuccessful
-    console.error('Falha ao autenticar ', msg);
-});
+// const client = new Client({
+//     puppeteer: {
+//         executablePath: '/usr/bin/brave-browser-stable',
+//     },
+//     authStrategy: new LocalAuth({
+//         clientId: "client-one"
+//     }),
+//     puppeteer: {
+//         headless: true,
+//     }
+// });
 
-client.on('ready', () => {
-    console.log('Cliente pronto');
-    clientOn = "red";
-});
+// client.on('qr', qr => {
+//     //qrcode.generate(qr, {small: true});
+//     qrCode = qr;
+//     console.log("QR. CODE IS RUNNING")
+// });
 
-client.initialize();
+// client.on('loading_screen', (percent, message) => {
+//     console.log('Carregando... ', percent, message);
+//     percentual = percent;
+//     message = message;
+// });
 
-app.get('/qrcode', (req, res, next) => {
+// client.on('authenticated', () => {
+//     console.log('Autenticado');
+//     auth = true;
+// });
 
-    //Gerar o conteúdo do QR Code
-    const qrCodeContent = qrCode;
+// client.on('auth_failure', msg => {
+//     // Fired if session restore was unsuccessful
+//     console.error('Falha ao autenticar ', msg);
+// });
 
-    // Gerar o código QR baseado no conteúdo
-    QRCODE.toDataURL(qrCodeContent, (err, url) => {
-        if (err) {
-            console.error('Erro ao gerar o código QR:', err);
-            res.status(500).send('Erro ao gerar o código QR');
-        } else {
-            // console.log("url: " + url);
-            // Exibir o código QR no navegador
+// client.on('ready', () => {
+//     console.log('Cliente pronto');
+//     clientOn = "red";
+// });
+
+// client.initialize();
+
+// app.get('/qrcode', (req, res, next) => {
+
+//     //Gerar o conteúdo do QR Code
+//     const qrCodeContent = qrCode;
+
+//     // Gerar o código QR baseado no conteúdo
+//     QRCODE.toDataURL(qrCodeContent, (err, url) => {
+//         if (err) {
+//             console.error('Erro ao gerar o código QR:', err);
+//             res.status(500).send('Erro ao gerar o código QR');
+//         } else {
+//             // console.log("url: " + url);
+//             // Exibir o código QR no navegador
     
-            // res.send(qrCodeHtml);
-            res.status(200).json({
-                qrCode: url,
-                percentual: percentual,
-                message: message,
-                auth: auth,
-                auth_error: auth_error,
-                clientOn: clientOn
+//             // res.send(qrCodeHtml);
+//             res.status(200).json({
+//                 qrCode: url,
+//                 percentual: percentual,
+//                 message: message,
+//                 auth: auth,
+//                 auth_error: auth_error,
+//                 clientOn: clientOn
 
-            })
-        }
-    });
+//             })
+//         }
+//     });
 
-});
+// });
 
 
-module.exports = app;
+// module.exports = app;
 
 
 
