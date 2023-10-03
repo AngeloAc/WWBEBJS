a088c0f29ddafaaa51f67959455a78bc5b22e2b3
AngeloAc <euclidescarvalho1904@gmail.com>
2023-10-03 17:25:30 +0100

    update

--- src/teste.js
+++ src/teste.js
@@ -26,37 +26,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// Rota para receber o upload de imagens
-// app.post('/upload', (req, res) => {
-// //   if (!req.file) {
-// //     return res.status(400).send('Nenhum arquivo foi enviado.');
-// //   }
-//     console.log(req.body)
-//   res.send('Arquivo enviado com sucesso!');
-// });
-
-
-
-
-// const qrcode = require('qrcode-terminal');
-
-// const { Client } = require('whatsapp-web.js');
-// const client = new Client();
-
-// client.on('qr', qr => {
-//     qrcode.generate(qr, {small: true});
-// });
-
-// client.on('ready', () => {
-//     console.log('Client is ready!');
-// });
-
-// client.on('loading_screen', (percent, message) => {
-//     console.log('Carregando... ', percent, message);
-// });
-
-// client.initialize();
-
 
 const { Client, LocalAuth ,MessageMedia, Buttons } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
@@ -78,35 +47,60 @@ client.on('loading_screen', (percent, message) => {
 });
 
 
-client.on('message',async message => {
-    console.log('message from', message.from)
-    console.log(message)
+// client.on('message',async message => {
+//     console.log('message from', message.from)
+//     console.log(message)
     
-                                if (message.hasMedia) {
-                            const time = new Date(message.timestamp * 1000).toISOString().replace(/T/, ' ').replace(/\..+/, '').split(' ')[1].replaceAll(':', '-')
-                            const date = new Date(message.timestamp * 1000).toISOString().substring(0, 10);
-                            const person = message['_data']['notifyName'];
-                            const phoneNumber = message.from.replaceAll('@c.us', '');
-                            const media = await message.downloadMedia();
-                            console.log(media)
-                            // do something with the media data here
-                            const folder = process.cwd() + '/img/' + phoneNumber + '_' + person + '/' + date + '/';
-                            const filename = folder + time + '_' + message.id.id + '.' + media.mimetype.split('/')[1];
-                            fs.mkdirSync(folder, { recursive: true });
-                            fs.writeFileSync(filename, Buffer.from(media.data, 'base64').toString('binary'), 'binary');
-                            if (media.mimetype === 'audio/ogg; codecs=opus') {
-                                console.log('AUDIO SENT: ');
+//                                 if (message.hasMedia) {
+//                             const time = new Date(message.timestamp * 1000).toISOString().replace(/T/, ' ').replace(/\..+/, '').split(' ')[1].replaceAll(':', '-')
+//                             const date = new Date(message.timestamp * 1000).toISOString().substring(0, 10);
+//                             const person = message['_data']['notifyName'];
+//                             const phoneNumber = message.from.replaceAll('@c.us', '');
+//                             const media = await message.downloadMedia();
+//                             console.log(media)
+//                             // do something with the media data here
+//                             const folder = process.cwd() + '/img/' + phoneNumber + '_' + person + '/' + date + '/';
+//                             const filename = folder + time + '_' + message.id.id + '.' + media.mimetype.split('/')[1];
+//                             fs.mkdirSync(folder, { recursive: true });
+//                             fs.writeFileSync(filename, Buffer.from(media.data, 'base64').toString('binary'), 'binary');
+//                             if (media.mimetype === 'audio/ogg; codecs=opus') {
+//                                 console.log('AUDIO SENT: ');
 
-                            } else if (media.mimetype === 'image/jpeg') {
-                                console.log("IMAGE SENT: ");
-                            }
+//                             } else if (media.mimetype === 'image/jpeg') {
+//                                 console.log("IMAGE SENT: ");
+//                             }
                      
         
 
-    }
+//     }
+// });
+
+app.post('/sendMessage', (req, res, next) => {
+  console.log("estou recebendo a message...");
+  try {
+      // console.log(req.body);
+      const phone = req.body.phone_invite;
+      const sender = req.body.sender;
+      const message_invite = "Foste convidado pelo seu amigo(a) " + sender + " juntar-te a família Startic, com a melhor Inteligencia Artificial. Acesse também: https://startic.ao. Para saber mais podemos conversar aqui.";
+      client.sendMessage(`${req.body.index}${phone}@c.us`, message_invite).then((message) => {
+          // console.log(`Message sent successfully. Message ID: `);
+
+      })
+          .catch((error) => {
+              console.error('Error sending message:', error);
+              return res.status(400).json({ error: "Não conseguimos enviara a sua mensagem." });
+          });
+
+      return res.status(200).json({
+          message: "Mensagem enviada."
+      });
+  } catch (error) {
+      return res.status(400).json({
+          error: error
+      })
+  }
+
 });
-
-
 
 
 // Rota para receber o upload de imagens
@@ -119,17 +113,13 @@ app.post('/upload', upload.single('file'), (req, res) => {
     res.send('Arquivo enviado com sucesso!');
   });
 
+  app.post('/verificationcode', (req, res) => {
+    client.sendMessage(`${req.body.index}${req.body.telefone}@c.us`, `O codigo *${req.body.randomNumber}* será utilizado na app Chat.Startic `);
+    return res.status(200).json({
+      data: req.body
+    })
+  });
 
-
-// app.post('/upload', (req, res) => {
-//     //   if (!req.file) {
-//     //     return res.status(400).send('Nenhum arquivo foi enviado.');
-//     //   }
-//         console.log(req.body)
-//         client.sendMessage("244935407576@c.us", req.body);
-//       res.send('Arquivo enviado com sucesso!');
-//     });
-    
  
 client.initialize();
  
